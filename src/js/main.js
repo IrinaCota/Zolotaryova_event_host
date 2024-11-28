ed005754eba8b6f4973e14adf2a8c73f215c3d99
IrinaCota <iryna.kotsa91@gmail.com>
2024-11-28 11:10:12 +0000

    up

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+//import './js/about-me';
+//import './js/aos';
+
+//import './js/footer';
+import './header';
+import './modal';
+
+import './reviews';
+import './scroll-to-top';
+//import './js/theme-picker';
+import './swiper';
+
+import 'swiper/swiper-bundle.css';
+import '../css/styles.css'; // Імпорт стилів
+
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,15 @@
 import { defineConfig } from 'vite';
-import { sync } from 'glob'; // Зміна імпорту
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig({
+  base: './', // Використовуйте відносний базовий шлях
+  build: {
+    outDir: './dist',
+    rollupOptions: {
+      input: './index.html',
     },
-    root: '.', // Встановлюємо корінь проекту на головну папку
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: sync('./*.html'), // Тепер ми вказуємо, що основний HTML файл в корені
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
-      },
-      outDir: './dist',
-    },
-    plugins: [injectHTML(), FullReload(['./index.html'])], // Додаємо слідкування за змінами в index.html
-
-    server: {
-      open: '/index.html', // Це відкриє index.html при старті серверу
-    },
-  };
+  },
+  publicDir: 'public', // Вказує на папку зі статичними файлами
+  server: {
+    open: '/index.html',
+  },
 });
